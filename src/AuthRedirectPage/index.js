5b550d9109de8f1a8b8fd8eb125ee604f88826cb
james tak <jtak93@gmail.com>
2017-09-20 00:32:51 -0700

    adjust

--- src/AuthRedirectPage/index.js
+++ src/AuthRedirectPage/index.js
@@ -27,7 +27,7 @@ export class AuthRedirectPage extends React.Component { // eslint-disable-line r
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
-        params: body,
+        data: JSON.stringify(body),
       }
       axios.post('https://www.bungie.net/Platform/App/OAuth/token/', {}, axiosConfig)
         .then(resp => {
